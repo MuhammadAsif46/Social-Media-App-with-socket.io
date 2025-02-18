3f29ab7f54ed9dd8df2290ce919a1f74b34032d7
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-02-18 12:24:17 +0500

    update

--- mongodb.mjs
+++ mongodb.mjs
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 import OpenAI from 'openai';                
 
 //define uri mongodb cluster
-const uri = "mongodb+srv://dbuser:dbpassword@cluster0.ovydsoh.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbuser:asif123@cluster0.ovydsoh.mongodb.net/?retryWrites=true&w=majority";
 export const client = new MongoClient(uri);
 
 
